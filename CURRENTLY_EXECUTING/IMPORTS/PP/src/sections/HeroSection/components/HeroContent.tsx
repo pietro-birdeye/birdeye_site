661964e4e49188ae6d9dc2a0851a1b7c300fed3a
Pietro Birdeye <pietro@birdeye.com>
2025-12-02 11:03:06 -0800

    chore: stop tracking temporary CurrentlyExecuting dumps

--- CURRENTLY_EXECUTING/IMPORTS/PP/src/sections/HeroSection/components/HeroContent.tsx
+++
@@ -1,9 +0,0 @@
-import { HeroText } from "@/sections/HeroSection/components/HeroText";
-
-export const HeroContent = () => {
-  return (
-    <div className="box-border caret-transparent col-end-[-2] col-start-2 z-[1] pt-[60px] md:pt-[44.8px]">
-      <HeroText />
-    </div>
-  );
-};
